41f5120176446ef039e22d7391078caf753b1f4e
Santaravind <aravindsant1310@gmail.com>
2026-04-21 18:17:42 +0530

    Case study categories add !!

--- src/components/pages/home/Learning.jsx
+++ src/components/pages/home/Learning.jsx
@@ -299,7 +299,7 @@ export default function LearningResources() {
   </div>
 
   {/* Divider */}
-  <div className="h-[1px] bg-slate-100 mb-4"></div>
+  <div className="h-px bg-slate-100 mb-4"></div>
 
   {/* Action Buttons */}
   <div className="flex items-center justify-between">
@@ -313,7 +313,7 @@ export default function LearningResources() {
     </button>
 
     {/* Buy Now */}
-    <button className="relative px-5 py-2 text-sm font-semibold text-white rounded-xl bg-gradient-to-r from-indigo-600 to-blue-600 shadow-md hover:shadow-lg hover:scale-[1.02] active:scale-95 transition-all duration-200">
+    <button className="relative px-5 py-2 text-sm font-semibold text-white rounded-xl bg-linear-to-r from-indigo-600 to-blue-600 shadow-md hover:shadow-lg hover:scale-[1.02] active:scale-95 transition-all duration-200">
       BUY NOW
     </button>
 

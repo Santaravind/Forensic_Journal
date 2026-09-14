bc23c844d0330f21670fc046ff579ba218a18c7f
Santaravind <aravindsant1310@gmail.com>
2026-09-14 13:52:48 +0530

    Some changes

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
       <div className="flex flex-col sm:flex-row items-center lg:gap-2 md:gap-0.5 text-white">
         <p className="flex items-center gap-2 text-lg md:text-[16px]">
           <FaPhoneAlt  className="text-yellow-300" />
-          <span>+91-8957445211</span>
+          <span>+91-XXXXXXX</span>
         </p>
   
         <p className="flex items-center gap-2 text-lg md:text-[16px]">
